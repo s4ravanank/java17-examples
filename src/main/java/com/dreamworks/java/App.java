7efb7a28b637485d40d0f39354c81908864931f3
Saravanan Kandaswamy <s4ravanank@gmail.com>
2023-01-15 20:27:01 +0530

    Added Adam-Bien's short video code

--- src/main/java/com/dreamworks/java/App.java
+++ src/main/java/com/dreamworks/java/App.java
@@ -1,13 +1,42 @@
 package com.dreamworks.java;
 
 
+import java.util.List;
+
 /**
  *
  * @author saravanan k
  */
 public class App {
 
+    public static void checkIfPresentOrNot(){
+        var languages = List.of("java", "js");
+        var empty = languages
+                .stream().filter(l->l.equals("typescript")).findAny().isEmpty();
+        System.out.println(empty);
+    }
+
+    public static void stringFormatReuse(){
+        var message = """
+                hello, %1$s,
+                question: %2$s
+                good bye %1$s
+                """.formatted("duke", "are you okay?");
+        System.out.println(message);
+        System.out.printf("Hello %s \n", "duke");
+        System.out.format("Hello %s \n", "duke");
+    }
+
+    public static void consoleInput(){
+        var console = System.console();
+        console.printf(">");
+        var input = console.readLine();
+        console.printf("echo: "+ input);
+    }
+
     public static void main(String[] args) {
-        System.out.println("is working");
+        //checkIfPresentOrNot();
+        //stringFormatReuse();
+//        consoleInput();
     }
 }
